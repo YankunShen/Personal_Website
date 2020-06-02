47837198216d9e560fe5163153b606444ba65d8a
YankunShen <yankun@uw.edu>
2020-06-02 14:35:50 -0700

    Updates

---
+++ precache-manifest.d7dd3be8fa0b7f47c61bf6ffbe5ddec9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b536700d2ba41b8d595eb80c83665d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf258c9fab85522bb85",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "01e19a10f4488977302e",
+    "url": "/static/css/main.b9ab4fe4.chunk.css"
+  },
+  {
+    "revision": "6bf258c9fab85522bb85",
+    "url": "/static/js/2.fcc4fef2.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/static/js/2.fcc4fef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e19a10f4488977302e",
+    "url": "/static/js/main.8e3a55ba.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "3f4a692b30bd89b23cdd2266a72d546f",
+    "url": "/static/media/resume.3f4a692b.pdf"
+  }
+]);
