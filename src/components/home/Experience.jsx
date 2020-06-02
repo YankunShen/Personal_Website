965462ff5f6391795fa6c2ee3755c584a943773c
YankunShen <yankun@uw.edu>
2020-06-02 16:17:41 -0700

    fix resizing page

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -5,12 +5,12 @@ import Timeline from "./Timeline";
 
 function Experience() {
   return (
-      <div className="jumbotron jumbotron-fluid bg-transparent m-0" id="experience">
-        <div className="container container-fluid p-5">
-          <h1 className="display-4 pb-5">Experience</h1>
-            <Timeline />
-          </div>
-      </div>
+    <section className="section" id="experience">
+    <div className="container">
+      <h1 className="title">Experience</h1>
+      <Timeline />
+    </div>
+  </section>
   );
 }
 
